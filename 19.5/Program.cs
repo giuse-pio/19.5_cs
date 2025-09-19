43fcc754f6a94e8bc1d7db19b858979d4c6f9034
giupe <giupe@MSI>
2025-09-19 17:53:54 +0200

    Add project files.

---
+++ 19.5/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _19._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (int n in numbers)
+            {
+
+                Console.WriteLine(n);
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int n = numbers[i];
+                if (n % 2 != 0)
+                {
+                    numbers.Remove(n);
+                }
+            }
+
+            Console.WriteLine();
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
